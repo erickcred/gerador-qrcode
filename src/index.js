0b6a22490bc73fef65084122937b3e044f69f011
Erick Rick <erickcredjes@gmail.com>
2025-06-29 16:35:53 -0300

    Refatoração

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import prompt from 'prompt';
 
-import mainPrompt from './prompts/prompt-main.js';
+import promptSchemaMain from './prompts-schema/prompt-schema-main.js';
 import createQRCode from './services/qr-code/create.js';
 import createPassword from './services/password/create.js'
 
 async function main() {
   console.clear();
-  prompt.get(mainPrompt, async (err, result) => {
+  prompt.get(promptSchemaMain, async (err, result) => {
     
     if (err) {
       console.clear();
       return;
     }
 
-    if (result.select == '1') 
-      await createQRCode();
+    if (result.select == '1') await createQRCode();
 
-    if (result.select == '2')
-      await createPassword();
+    if (result.select == '2') await createPassword();
   });
   prompt.start();
 
--- src/prompts-schema/prompt-schema-main.js
+++ src/prompts-schema/prompt-schema-main.js
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 
-const mainPrompt = [
+const promptSchemaMain = [
   {
     name: 'select',
     description: chalk.cyan.bold('Escolha a ferramenta (1 - QRCode, 2 - Password)'),
@@ -10,4 +10,4 @@ const mainPrompt = [
   },
 ]
 
-export default mainPrompt;
+export default promptSchemaMain;
